4581e285687500175412d20964d4a181243f66bc
Aidan Doak <hc.adoak@gmail.com>
2017-08-13 13:44:49 +1200

    Search string from the index field is able to be used by the query function of results

--- web/js/main.js
+++ web/js/main.js
@@ -1,24 +1,3 @@
-
-function fetchBookmarks(){
-  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-
-  var bookmarksResults = document.getElementById('bookmarksResults');
-
-  bookmarksResults.innerHTML = '';
-
-  for(var i = 0; i < bookmarks.length; i++){
-    var name = bookmarks[i].name;
-    var url = bookmarks[i].url;
-
-    bookmarksResults.innerHTML += '<div class="well">'+
-                                  '<h3>'+name+
-                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
-                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
-                                  '</h3>'+
-                                  '</div>';
-  }
-}
-
 function queryBackend(query){
   var httpGet;
 
@@ -40,4 +19,14 @@ function queryBackend(query){
 
 function renderResults(results){
   console.log(results);
+}
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
