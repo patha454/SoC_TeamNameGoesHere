1ff264c045cbc7c6a0c2aabc13f384dd60301c68
Aidan Doak <hc.adoak@gmail.com>
2017-08-13 12:42:48 +1200

    JSON retrieval function added, render currently not working.

--- web/js/main.js
+++ web/js/main.js
@@ -1,56 +1,3 @@
-//listen for form submit
-document.getElementById('myForm').addEventListener('submit', saveBookmark);
-
-//save bookmark
-function saveBookmark(e){
-	
-	
-  //get form values
-	var siteName = document.getElementById('siteName').value;
-  //var siteUrl = document.getElementById('siteURL').value;
-	alert(siteName);
-  if (!validateForm(siteName, siteUrl)){
-    return false;
-  }
-
-  var bookMark = {
-    name: siteName,
-    url: siteUrl
-  }
-
-
-  if(localStorage.getItem('bookmarks') === null){
-    bookmarks = [];
-
-    bookmarks.push(bookMark);
-
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-  } else {
-    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-
-    bookmarks.push(bookMark);
-
-    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-  }
-
-  document.getElementById('myForm').reset();
-  fetchBookmarks();
-  //prevents form from submitting
-  e.preventDefault();
-}
-
-function deleteBookmark(url){
-  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-
-  for(var i = 0; i < bookmarks.length;i++){
-    if(bookmarks[i].url == url){
-      bookmarks.splice(i, 1);
-    }
-  }
-  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-
-  fetchBookmarks();
-}
 
 function fetchBookmarks(){
   var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
@@ -72,19 +19,25 @@ function fetchBookmarks(){
   }
 }
 
-function validateForm(siteName, siteUrl){
-  if(!siteName || !siteUrl){
-    alert('Please fill in details');
-    return false;
+function queryBackend(query){
+  var httpGet;
+  console.log("query")
+
+  function retrieveUnifyJSON(){
+    httpGet = new XMLHttpRequest();
+
+    httpGet.onreadystatechange = loadJSON;
+    httpGet.open('GET', 'http://127.0.0.1/?search='+query);
+    httpGet.send();
   }
 
-  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-  var regex = new RegExp(expression);
-
-  if (!siteUrl.match(regex)) {
-    alert("Please use a valid URL");
-    return false;
+  function loadJSON(){
+    var queryJSON = JSON.parse(httpGet.responseText);
+    console.log(queryJSON);
+    renderResults(queryJSON);
   }
+}
 
-  return true;
+function renderResults(results){
+  console.log(results);
 }
